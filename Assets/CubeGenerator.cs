c4953ad808464397b8eddc11ae14c38daa0b5e30
yu-tan <yu.tan10714@gmail.com>
2022-02-24 00:21:13 +0900

    shooting完成

---
+++ Assets/CubeGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Create());
+    }
+
+    IEnumerator Create()
+    {
+        float delta = 1.5f;
+        while (true)
+        {
+            GameObject obj = Instantiate(
+                prefab,
+                new Vector3(Random.Range(-12.0f,12.0f),Random.Range(8.0f,12.0f),Random.Range(-3.0f,3.0f)),
+                Quaternion.identity
+                );
+
+            obj.GetComponent<CubeController>().SetGameManager(gm);
+            yield return new WaitForSeconds(delta);
+            if(delta > 0.5f)
+            {
+                delta -= 0.05f;
+            }
+
+        }
+    }
+}
